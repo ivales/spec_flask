ebf1020265d817d9f8c97a01c782447e1455c393
ivales <i.leskin@mail.ru>
2023-04-15 15:17:53 +0300

    problem with last element

--- app.py
+++ app.py
@@ -1,144 +1,65 @@
-from flask import Flask, request, jsonify, abort
-from pathlib import Path
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+import random
 
-BASE_DIR = Path(__file__).parent
+from flask import Flask, request
+
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 
 
-app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'quotes.db'}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+quotes = [
+   {
+       "id": 3,
+       "author": "Rick Cook",
+       "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+   },
+   {
+       "id": 5,
+       "author": "Waldi Ravens",
+       "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+   },
+   {
+       "id": 6,
+       "author": "Mosher's Law of Software Engineering",
+       "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+   },
+   {
+       "id": 8,
+       "author": "Yoggi Berra",
+       "text": "В теории, теория и практика неразделимы. На практике это не так."
+   },
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-class AuthorModel(db.Model):
-    __tablename__ = 'authors'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
-    quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic')
-
-    def __init__(self, name):
-        self.name = name
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name
-        }
-
-
-class QuoteModel(db.Model):
-    __tablename__ = "quotes"
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey(AuthorModel.id))
-    text = db.Column(db.String(255), unique=False)
-    rating = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, author, text, rating=1):
-        self.author_id = author.id
-        self.text = text
-        self.rating = rating
-
-    def __repr__(self):
-        return f'Quote({self.author.name}, {self.text}, {self.rating})'
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "author": self.author.to_dict(),
-            "text": self.text,
-            "rating": self.rating
-        }
-
-# Resource: Author
-
-
-@app.route("/authors")
-def get_authors():
-    pass
-
-
-@app.route("/authors/<int:author_id>")
-def get_author_by_id(author_id):
-    pass
-
-
-@app.route("/authors", methods=["POST"])
-def create_author():
-    author_data = request.json
-    author = AuthorModel(author_data["name"])
-    db.session.add(author)
-    db.session.commit()
-    return jsonify(author.to_dict()), 201
-
-
-# Resource Quote
-@app.route("/quotes")
-def get_all_quotes():
-    """ Сериализация list[quotes] --> list[dict] --> str(JSON)"""
-    quotes = QuoteModel.query.all()
-    quotes_as_dict = []
+]
+#Task 1-1
+@app.route("/quotes/<int:id>")
+def get_quote(id):
     for quote in quotes:
-        quotes_as_dict.append(quote.to_dict())
-    return jsonify(quotes_as_dict), 200
+        if quote["id"] == id:
+            return quote
+#Task 1-2
+    return f"Quote with id={id} not found", 404
 
+#Task1-3
+@app.route("/quotes/count")
+def count_quotes():
+    return {
+            "count":len(quotes)
+            }, 200
 
-@app.route("/quotes/<int:quote_id>")
-def get_quote_by_id(quote_id):
-    quote = QuoteModel.query.get(quote_id)
-    if quote is None:
-        abort(404, f"Quote with id={quote_id} not found")
-    return jsonify(quote.to_dict()), 200
+#Task1-4
+@app.route("/quotes/random")
+def random_quote():
+    return random.choice(quotes), 200
 
+#Task2-1
+@app.route("/quotes", methods=['POST'])
+def create_quote():
+   data = request.json
+   print("data = ", data)
+   quotes.append({"index":last_quote_id()+1, "author": data["author"], "text":data["text"]})
+   return get_quote(last_quote_id()), 201
 
-@app.route("/authors/<int:author_id>/quotes", methods=["POST"])
-def create_quote(author_id):
-    author = AuthorModel.query.get(author_id)
-    new_quote = request.json
-    q = QuoteModel(author, **new_quote)
-    db.session.add(q)
-    db.session.commit()
-    return jsonify(q.to_dict()), 201
-
-
-@app.put('/quotes/<int:quote_id>')
-def edit_quote(quote_id):
-    new_quote = request.json
-    quote = QuoteModel.query.get(quote_id)
-    if quote is None:
-        abort(404, f"Quote with id={quote_id} not found")
-    # if new_quote.get('author'):
-    #     quote.author = new_quote['author']
-    # if new_quote.get('text'):
-    #     quote.text = new_quote['text']
-    for key, value in new_quote.items():
-        setattr(quote, key, value)
-    db.session.commit()
-    return jsonify(quote.to_dict()), 200
-
-
-@app.route("/quotes/<int:quote_id>", methods=["DELETE"])
-def delete_quote(quote_id):
-    quote = QuoteModel.query.get(quote_id)
-    if quote is None:
-        abort(404, f"Quote with id={quote_id} not found")
-    db.session.delete(quote)
-    db.session.commit()
-    return jsonify({"message": f"Quote with id={quote_id} has deleted"}), 200
-
-
-@app.route("/quotes/filter")
-def get_quotes_by_filter():
-    # kwargs = request.args
-    # quotes = QuoteModel.query.filter_by(**kwargs).all()
-    # result = [quote.to_dict() for quote in quotes]
-    # return jsonify(result)
-    return jsonify([q.to_dict() for q in QuoteModel.query.filter_by(**request.args).all()])
-
+def last_quote_id():
+    return quotes[-1]["id"]
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(debug=True)
