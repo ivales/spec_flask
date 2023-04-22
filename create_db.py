d91a98471894b127e8151217adf3fc9de0432b9a
ivales <i.leskin@mail.ru>
2023-04-22 15:34:51 +0300

    Tables_creation

--- create_db.py
+++ create_db.py
@@ -15,41 +15,72 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class AuthorModel(db.Model):
+    __tablename__ = "authors"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+
+
 class QuoteModel(db.Model):
-    id = db.Column(Integer, primary_key=True)
-    author = db.Column(String(32), unique=False)
+    __tablename__ = "quotes"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey(AuthorModel.id))
     text = db.Column(db.String(255), unique=False)
     rating = db.Column(db.Integer, unique=False, default=1)
 
     def __init__(self, author, text, rating):
-        self.author = author
+        self.author_id = author.id
         self.text = text
         self.rating = rating
 
 
 app.app_context().push()
 
-quotesForQuoteModel = [QuoteModel('Народная мудрость', 'Нет пламя без огня', 1),
-                       QuoteModel('Rick Cook',
+quotesForQuoteModel = [QuoteModel(AuthorModel("Nick"), 'Нет пламя без огня', 1),
+                       QuoteModel(AuthorModel("Folk"),
                                   'Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает.',
                                   4),
-                       QuoteModel('Waldi Ravens',
+                       QuoteModel(AuthorModel("Ralph"),
                                   'Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках.',
                                   1),
-                       QuoteModel('Moshers Law of Software Engineering',
+                       QuoteModel(AuthorModel("Waldo"),
                                   'Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили.', 3),
-                       QuoteModel('Yoggi Berra', 'В теории, теория и практика неразделимы. На практике это не так.', 2),
-                       QuoteModel('Test', 'Съешь еще этих французских булок, да выпей чаю', 5)]
+                       QuoteModel(AuthorModel("Tom"), 'В теории, теория и практика неразделимы. На практике это не так.', 2),
+                       QuoteModel(AuthorModel("Emerson"), 'Съешь еще этих французских булок, да выпей чаю', 5)]
+
+authorsForAuthorModel = ['Nick', 'Folk', 'Tom', 'Ralph', 'Emerson']
+
 
 engine = create_engine(f"sqlite:///{BASE_DIR / 'test.db'}")
-if not inspect(engine).has_table("quote_model"):  # If table don't exist, Create.
+
+
+if not inspect(engine).has_table("authors"):
+    metadata = MetaData(engine)
+    Table("authors", metadata,
+          Column('id', Integer, primary_key=True, nullable=False),
+          Column('name', String(32)))
+    metadata.create_all()
+
+
+if not inspect(engine).has_table("quotes"):  # If table don't exist, Create.
     metadata = MetaData(engine)
     # Create a table with the appropriate Columns
-    Table("quote_model", metadata,
+    Table("quotes", metadata,
           Column('id', Integer, primary_key=True, nullable=False),
-          Column('author', String(32)), Column('text', String(255)),
+          Column('author_id', Integer), Column('text', String(255)),
           Column('rating', Integer))
     metadata.create_all()
+
+
+if len(AuthorModel.query.all()) == 0:
+    for author in authorsForAuthorModel:
+        db.session.add(author)
+    db.session.commit()
+
 
 if len(QuoteModel.query.all()) == 0:
     for quote in quotesForQuoteModel:
